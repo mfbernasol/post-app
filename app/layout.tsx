f73bbac82f6962c53ab45a6d2f8ee168eeec2890
mfbernasol <mfbernasol@gmail.com>
2023-06-07 22:03:12 -0500

    setup next auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
-import './globals.css'
+import "./globals.css";
+import Nav from "./auth/Nav";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +20,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body
+        className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}
+      >
+        <Nav />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
